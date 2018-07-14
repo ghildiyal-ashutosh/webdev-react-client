98305156acd23777d2d5b1747fbce161a7ed97fa
ashu95 <ghildiyal.a@husky.neu.edu>
2018-07-13 21:26:46 -0400

    First Commit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'   // used for web applications
 
 ReactDOM.render(
-    <h1> Homepage For Teachers !!!</h1>,
+    <h1> Homepage For Teachers !!!!</h1>,
     document.getElementById('root')
 );
