b30b72e7dbc03e88655219bfc37de2feb7c395fa
ashu95 <ghildiyal.a@husky.neu.edu>
2018-07-13 21:20:12 -0400

    Index Page Created

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'   // used for web applications
+
+ReactDOM.render(
+    <h1> Homepage For Teachers !!!</h1>,
+    document.getElementById('root')
+);
