c9ed4485d0e85cf16626acd03b146016c2ab2c7a
ashu95 <ghildiyal.a@husky.neu.edu>
2018-07-22 19:33:26 -0400

    Url

--- src/services/CourseService.js
+++ src/services/CourseService.js
@@ -1,6 +1,6 @@
 let _singleton = Symbol();
 //const COURSE_API_URL = 'http://localhost:8080/api/course';
-const COURSE_API_URL  = 'http://react-client-ashu95.herokuapp.com/api/course'
+const COURSE_API_URL  = 'https://webdev-2.herokuapp.com/api/course'
 
 export default class CourseService
 {
